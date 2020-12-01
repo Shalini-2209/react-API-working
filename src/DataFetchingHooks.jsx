a18616183fc601f0de42d5ad2ecb71f3bb5c87fe
Ahana <ahana298@gmail.com>
2020-12-01 12:46:35 +0530

    Commit

--- src/DataFetchingHooks.jsx
+++ src/DataFetchingHooks.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Alert } from "react-bootstrap";
+import { Table, Alert } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const DataFetching = () => {
