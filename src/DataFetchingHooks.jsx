895bc6d23fdeaaee298f37fc447e291310e1e816
Ahana <ahana298@gmail.com>
2020-12-03 21:35:50 +0530

    Choose Individual record

--- src/DataFetchingHooks.jsx
+++ src/DataFetchingHooks.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Table, Alert } from "react-bootstrap";
+import { Table, Alert, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const DataFetching = () => {
-  const [list, setList] = useState([]);
+  const [list, setList] = useState({});
+  const [userId, setUserId] = useState(1);
+  const [btnData, setBtnData] = useState(1);
 
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/todos")
+      .get(`https://jsonplaceholder.typicode.com/todos/${userId}`)
       .then((res) => {
         console.log(res);
         setList(res.data);
@@ -16,13 +18,30 @@ const DataFetching = () => {
       .catch((err) => {
         console.log("Error", err);
       });
-  }, []);
+  }, [btnData]);
 
   return (
     <div>
       <Alert variant="primary">
-        <center>TO DO LIST | SORTED</center>{" "}
+        <center>TO DO LIST | SEARCH VIA ENTERING USER ID</center>{" "}
       </Alert>
+
+      <br />
+      <br />
+
+      <center>
+        <input
+          type="number"
+          value={userId}
+          onChange={(e) => setUserId(e.target.value)}
+          placeholder="Enter task id:"
+        />
+        <br />
+        <br />
+        <Button variant="success" onClick={() => setBtnData(userId)}>
+          Get Records
+        </Button>{" "}
+      </center>
       {/* <ul>
         {list.map((item) => (
           <li key={item.id}>
@@ -31,21 +50,30 @@ const DataFetching = () => {
         ))}
       </ul> */}
 
+      <br />
+      <br />
+
       <Table striped bordered hover variant="dark">
         <thead>
           <tr>
-            <th>User Id</th>
+            <th>Task Id</th>
             <th>Title</th>
           </tr>
         </thead>
         <tbody>
-          {list.map((item) => (
+          {/* Render all values */}
+          {/* {list.map((item) => (
             <tr key={item.id}>
               <td>{item.userId}</td>
               <td>{item.title}</td>
             </tr>
           ))}
-          ;
+          ; */}
+
+          <tr key={list.id}>
+            <td>{list.id}</td>
+            <td>{list.title}</td>
+          </tr>
         </tbody>
       </Table>
     </div>
